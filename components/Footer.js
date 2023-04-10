d5b5eda27b6f3045eaa0ec1521c32dc9eed0581d
PaglaDasu <snehasishbabin2001@gmail.com>
2023-04-10 19:37:23 +0530

    google clone final build

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
         <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2">
-           <GlobeAsiaAustraliaIcon className="h-5 mr-1 text-green-700"/> Carbon neutral since 2007
+           <GlobeAsiaAustraliaIcon className="h-5 mr-1 text-green-700"/> Coded by PaglaDasu with ❤️
         </div>
         <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
             <p>Advertising</p>
